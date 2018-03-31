dcff0348261daa89498f5b6d576ffa9331cd48b4
aniacw <cwiek.ania@gmail.com>
2018-03-31 20:34:04 +0200

    Factory2

--- src/main/java/System/Main.java
+++ src/main/java/System/Main.java
@@ -2,7 +2,7 @@ package System;
 
 public class Main {
     public static void main(String[] args){
-        SystemFactory systemFactory = new SystemFactory();
+
         OS system = SystemFactory.getOS("Linux");
 
     }
